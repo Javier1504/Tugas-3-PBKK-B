d968644739cd63403a5ca72c3f56f3e8d6139c1b
Javier1504 <114123536+Javier1504@users.noreply.github.com>
2023-09-22 10:34:23 +0700

    Add files via upload
    Tugas 3 WPF PBKK B

---
+++ PBKK-B-Tugas 3/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPF_PBKK_B_Javier_Nararya_A_S_5025211245
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
